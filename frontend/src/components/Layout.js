1661818df8beb2f9c02884ced18f48a43b7738ce
imimran <engr.aih@gmail.com>
2020-12-03 16:20:49 +0600

    Header, Footer, Layout & React-Bootstrap

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Header from "./Header";
+import { Container } from "react-bootstrap";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Head>
+        <title>PROSHOP </title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header>
+        <Header></Header>
+      </header>
+
+      <main className="py-3">
+        <Container>{children}</Container>
+      </main>
+      <footer style={{ position: "absolute", bottom: 0, width: "100%" }}>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import styles from "../styles/Home.module.css";
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+export default function Home() {
+  return (
+    <div>
+      <main>
+        <h4>Welcome to Proshop</h4>
+      </main>
+    </div>
+  );
+}
