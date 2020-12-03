3f6c8b30a467af99de0b4c442431c8ffc02261f7
imimran <engr.aih@gmail.com>
2020-12-03 14:02:50 +0600

    add login functionality

--- backend/app.js
+++ backend/app.js
@@ -4,22 +4,25 @@ const sequelize = require('./utils/db')
 const cors = require('cors')
 const app = express()
 
+//Routers
 const userRouter = require('./routes/user/user')
+const authRouter = require("./routes/user/auth");
 
 
 
-
+//Middlewares
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(cors())
 
 
 app.use('/api/user', userRouter)
+app.use("/api/auth", authRouter);
 
 
 const port = process.env.PORT || 5000 ;
 
-
+//Connect Database
 (async () =>{
     await sequelize.sync({ alter: true })
     //await sequelize.sync()
