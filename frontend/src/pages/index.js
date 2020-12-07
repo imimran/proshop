d15a89c13b704c131cb7bb5a2712906b8ebc15d1
imimran <engr.aih@gmail.com>
2020-12-07 16:54:02 +0600

    display product

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import { Row, Col } from 'react-bootstrap'
-import products from '../utils/products'
-import Product from '../components/Product'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import axios from "axios";
+import Product from "../components/Product";
+
+const Home = ({ products }) => {
+  console.log(products.results.data);
+
+  return (
+    <>
+      <h1> Latest Products</h1>
+      {products && (
+        <Row>
+          {products.results.data.map((product, index) => (
+            <Col key={index} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
+    </>
+  );
+};
 
 
-const Home = () => {
-    return (
-        <>
-            <h1> Latest Products</h1>
-            <Row>
-                {products.map((product, index) => (
-                    <Col  key={index} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
-        </>
-    )
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:5000/api/product");
+  const data = await res.json();
+  return {
+    props: {
+      products: data,
+    },
+  };
 }
- 
-export default Home 
+
+export default Home;
