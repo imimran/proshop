dea3f40423f2f63c19e3dbc2cf7baecc38791100
imimran <engr.aih@gmail.com>
2020-12-09 17:51:02 +0600

    add field in product

--- frontend/src/pages/product/[id].js
+++ frontend/src/pages/product/[id].js
@@ -7,39 +7,48 @@ const Product = ({ product }) => {
   console.log(product);
   return (
     <>
-      <Row>
-        <Col md={6}>
-          <Card.Img
-            variant="top"
-            style={{ width: "70%", height:"90%" }}
-            src={`http://localhost:5000/uploads/${product.results.data.image}`}
-          />
-        </Col>
-        <Col md={3}>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <h3>{product.results.data.title}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.results.data.rating}
-                text={product.results.data.numReviews}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>Brand: {product.results.data.brand}</ListGroup.Item>
-            <ListGroup.Item>
-              Category: {product.results.data.category}
-            </ListGroup.Item>
-            <ListGroup.Item>Price: {product.results.data.price}</ListGroup.Item>
-            <ListGroup.Item>
-              Description: {product.results.data.description}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              In Stock: {product.results.data.inStock}
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
+      {product.results.data && (
+        <Row>
+          <Col md={6}>
+            <Card.Img
+              variant="top"
+              style={{ width: "70%", height: "90%" }}
+              src={`http://localhost:5000/uploads/${product.results.data.image}`}
+            />
+          </Col>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3>{product.results.data.title}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.results.data.rating}
+                  text={product.results.data.numReviews}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Brand: {product.results.data.brand}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Category: {product.results.data.categoryId}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Sub-Category: {product.results.data.subcategoryId}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Price: {product.results.data.price}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.results.data.description}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                In Stock: {product.results.data.inStock}
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
